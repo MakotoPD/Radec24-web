a4894236d9ea16ea4b9b10a1f3262b6e7c1da044
MakotoPD <makoto.kontakt@gmail.com>
2022-08-11 17:23:20 +0200

    create project

---
+++ components/Menu.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<h1 class="text-xl">Radec24</h1>
+	</div>
+</template>
+
+<style lang="scss">
+	h1{
+		color: red;
+	}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Menu/>
+</template>
+
+<script>
+import Menu from '~/components/Menu.vue';
+export default {
+    name: "IndexPage",
+    components: { Menu }
+}
+</script>
+
+
+<style lang="scss">
+*{
+  font-family: 'Poppins', sans-serif;
+}
+</style>
